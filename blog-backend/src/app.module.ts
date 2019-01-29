e9e8aab337c19b2ea965b4eb719b994386b5c311
yemiwebby <yemiwebby@gmail.com>
2019-01-29 05:08:46 +0100

    made adjustments to the module

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { BlogModule } from './blog/blog.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
-    BlogModule
+    BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
